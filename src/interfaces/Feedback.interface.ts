d1ab23c48ba6192dffa02b81dbcc84bc6aa4828b
samiulalimsaad <samiulalimsaad@gmail.com>
2024-06-29 01:53:28 +0600

    FeedbackInterface updated

--- src/interfaces/Feedback.interface.ts
+++ src/interfaces/Feedback.interface.ts
@@ -1,6 +1,9 @@
+import { UserInterface } from "./User.interface";
 import { CommonInterface } from "./common.interface";
 
 export interface FeedbackInterface extends CommonInterface {
-    user: string;
     content: string;
+    createdBy: UserInterface;
+    project: string;
+    document: string;
 }
