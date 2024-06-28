ee1a51eead4d4f3cfc6c0e1326badd5e0c419d64
samiulalimsaad <samiulalimsaad@gmail.com>
2024-06-29 02:49:04 +0600

    link issue fixed

--- src/utils/SOCKET.ts
+++ src/utils/SOCKET.ts
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
-import { BASE_URL } from "../config/siteConfig";
+import { BASE_URL_SOCKET } from "../config/siteConfig";
 
-export const SOCKET = io(BASE_URL);
+export const SOCKET = io(BASE_URL_SOCKET);
