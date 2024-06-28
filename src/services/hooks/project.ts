a8ee87bd7a558c890bc38949bcab5e600e03f928
samiulalimsaad <samiulalimsaad@gmail.com>
2024-06-28 17:55:43 +0600

    project `CRUD` with tanstack query

---
+++ src/services/hooks/project.ts
@@ -0,0 +1,63 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { ProjectInterface } from "../../interfaces/Project.interface";
+import {
+    createProject,
+    deleteProject,
+    fetchProjectById,
+    fetchProjects,
+    updateProject,
+} from "../api/project";
+
+export const useFetchProjects = () => {
+    return useQuery({
+        queryKey: ["projects"],
+        queryFn: fetchProjects,
+    });
+};
+
+export const useFetchProjectById = (id: string) => {
+    return useQuery({
+        queryKey: ["project", id],
+        queryFn: () => fetchProjectById(id),
+    });
+};
+
+export const useCreateProject = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: createProject,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["projects"] });
+        },
+    });
+};
+
+export const useUpdateProject = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: ({
+            id,
+            updatedProject,
+        }: {
+            id: string;
+            updatedProject: Partial<ProjectInterface>;
+        }) => updateProject(id, updatedProject),
+        onSuccess: (_, { id }) => {
+            queryClient.invalidateQueries({ queryKey: ["projects"] });
+            queryClient.invalidateQueries({ queryKey: ["project", id] });
+        },
+    });
+};
+
+export const useDeleteProject = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: (id: string) => deleteProject(id),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["projects"] });
+        },
+    });
+};
