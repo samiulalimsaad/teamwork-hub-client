60d6f81a099155c7f5d0442ac58dfd6891f1cff9
samiulalimsaad <samiulalimsaad@gmail.com>
2024-06-29 02:50:17 +0600

    temporary chat added

---
+++ src/components/Chat.tsx
@@ -0,0 +1,46 @@
+// src/App.tsx
+import React, { useEffect, useState } from "react";
+import { SOCKET } from "../utils/SOCKET";
+
+const Chat: React.FC = () => {
+    const [messages, setMessages] = useState<string[]>([]);
+    const [message, setMessage] = useState<string>("");
+
+    useEffect(() => {
+        SOCKET.on("receiveMessage", (message: string) => {
+            setMessages((prevMessages) => [...prevMessages, message]);
+        });
+
+        return () => {
+            SOCKET.disconnect();
+        };
+    }, []);
+
+    const sendMessage = () => {
+        if (message.trim()) {
+            SOCKET.emit("sendMessage", message);
+            setMessage("");
+        }
+    };
+
+    return (
+        <div>
+            <div>
+                {messages.map((msg, index) => (
+                    <div key={index}>{msg}</div>
+                ))}
+            </div>
+            <input
+                type="text"
+                value={message}
+                className="w-full textarea textarea-bordered"
+                onChange={(e) => setMessage(e.target.value)}
+            />
+            <button onClick={sendMessage} className="btn btn-block">
+                Send
+            </button>
+        </div>
+    );
+};
+
+export default Chat;
