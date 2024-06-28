7be15ea5b8992a4b9b899a878f262caff08f9481
samiulalimsaad <samiulalimsaad@gmail.com>
2024-06-29 01:54:30 +0600

    navbar applied

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,21 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import { useAuth } from "../providers/hooks/auth";
 
 const MainLayout = () => {
+    const { loading } = useAuth();
     return (
-        <div className="w-full mx-auto sm:w-3/5">
-            <Outlet />
+        <div>
+            <div className="bg-accent">
+                <Navbar />
+            </div>
+            <div className="w-full mx-auto sm:w-3/5">
+                {loading ? (
+                    <span className="loading loading-dots loading-lg"></span>
+                ) : (
+                    <Outlet />
+                )}
+            </div>
         </div>
     );
 };
