572c3bf67596d9e4f4254258833bc1f248086d57
samiulalimsaad <samiulalimsaad@gmail.com>
2024-06-28 22:09:55 +0600

    AuthProvider applied

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,19 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
+import AuthProvider from "./providers/AuthProvider.tsx";
 import { router } from "./routes/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <QueryClientProvider client={new QueryClient()}>
-            <RouterProvider router={router} />
+            <AuthProvider>
+                <RouterProvider router={router} />
+            </AuthProvider>
         </QueryClientProvider>
+        <ToastContainer />
     </React.StrictMode>
 );
