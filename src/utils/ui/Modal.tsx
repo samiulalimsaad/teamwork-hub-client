7b496e9b2122f6363e2dbfc72cfb359afe968968
samiulalimsaad <samiulalimsaad@gmail.com>
2024-06-29 16:50:33 +0600

    ui updated

--- src/utils/ui/Modal.tsx
+++ src/utils/ui/Modal.tsx
@@ -13,14 +13,14 @@ export const Modal: FC<ModalProps> = ({ title, isOpen, children }) => {
             <Dialog
                 open={isOpen}
                 as="div"
-                className="relative z-10 focus:outline-none "
+                className="relative z-10 focus:outline-none"
                 onClose={() => {}}
             >
-                <div className="fixed inset-0 z-10 w-screen overflow-y-auto backdrop-blur-lg">
-                    <div className="flex items-center justify-center min-h-full p-4 ">
+                <div className="fixed inset-0 z-10 w-screen overflow-y-auto backdrop-blur-sm">
+                    <div className="flex items-center justify-center min-h-full p-4 bg-neutral/20">
                         <DialogPanel
                             transition
-                            className="w-3/5 p-8 border shadow-md bg-accent/10 border-accent/10"
+                            className="w-3/5 p-8 bg-white border shadow-md border-accent/10 glass"
                         >
                             <DialogTitle
                                 as="h3"
