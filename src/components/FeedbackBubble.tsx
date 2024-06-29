a4dc363dded2a2f23ce5a7555dfa2db713c3353a
samiulalimsaad <samiulalimsaad@gmail.com>
2024-06-29 18:49:13 +0600

    FeedbackBubble by user

--- src/components/FeedbackBubble.tsx
+++ src/components/FeedbackBubble.tsx
@@ -1,21 +1,46 @@
 import moment from "moment";
 import { FeedbackInterface } from "../interfaces/Feedback.interface";
+import { useAuth } from "../providers/hooks/auth";
 
 interface FeedbackBubbleProps {
     feedback: FeedbackInterface;
 }
 
 const FeedbackBubble: React.FC<FeedbackBubbleProps> = ({ feedback }) => {
+    const { user } = useAuth();
+
     return (
         <div>
             <div className="chat chat-start">
+                <div className="chat-image avatar placeholder">
+                    <div
+                        className={`w-10 !rounded-full shadow-md ${
+                            user?._id === feedback.createdBy._id
+                                ? "bg-info text-info-content"
+                                : "bg-accent text-info-content"
+                        }`}
+                    >
+                        <span className="text-2xl">
+                            {feedback.createdBy.name[0].toUpperCase()}
+                        </span>
+                    </div>
+                </div>
+
                 <div className="chat-header">
                     {feedback.createdBy.name}
                     <time className="text-xs opacity-50">
                         {moment(feedback.createdAt).fromNow()}
                     </time>
                 </div>
-                <div className="chat-bubble">{feedback.content}</div>
+                <div
+                    className={`chat-bubble ${
+                        user?._id === feedback.createdBy._id
+                            ? "chat-bubble-info"
+                            : "chat-bubble-accent"
+                    }`}
+                >
+                    {feedback.content}
+                </div>
             </div>
         </div>
     );
