3b50cdb5d27b84e5f7c9c83e1c8fd1fd4a4b2bf6
samiulalimsaad <samiulalimsaad@gmail.com>
2024-06-29 16:20:20 +0600

    handle 401

--- src/utils/API.ts
+++ src/utils/API.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { BASE_URL } from "../config/siteConfig";
-import { useModalStore } from "../store";
+import { useModalStore } from "../store/modal";
+import { useUserStore } from "../store/user";
 
 export const API = axios.create({ baseURL: BASE_URL, withCredentials: true });
 
@@ -10,8 +11,8 @@ API.interceptors.response.use(
         if (error.response.status === 401) {
             console.error("Unauthorized: Redirecting to login...");
             useModalStore.getState().open();
+            useUserStore.getState().reset();
         } else if (error.response.status === 403) {
-            // Handle forbidden error (e.g., show a forbidden message)
             console.error("Forbidden: Access denied.");
             alert("You do not have permission to access this resource.");
         }
