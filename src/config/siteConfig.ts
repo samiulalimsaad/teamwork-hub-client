caeb44fcf58f0d2e53b40600788292453a70ca89
samiulalimsaad <samiulalimsaad@gmail.com>
2024-06-29 17:30:22 +0600

    updated

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -1,2 +1,5 @@
-export const BASE_URL = "http://localhost:5000/api";
-export const BASE_URL_SOCKET = "http://localhost:5000";
+// export const URL = "https://teamwork-hub-server.vercel.app";
+export const URL = "https://teamwork-hub-server.onrender.com";
+// export const URL = "http://localhost:5000";
+export const BASE_URL = `${URL}/api`;
+export const BASE_URL_SOCKET = URL;
