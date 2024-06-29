f11a051eb30bfc360146f71c590181e69615a8dd
samiulalimsaad <samiulalimsaad@gmail.com>
2024-06-29 12:43:46 +0600

    Modal added

---
+++ src/utils/ui/Modal.tsx
@@ -0,0 +1,41 @@
+import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import { FC, ReactNode } from "react";
+import { useModalStore } from "../../store";
+
+interface ModalProps {
+    title: string;
+    children: ReactNode;
+}
+
+export const Modal: FC<ModalProps> = ({ title, children }) => {
+    const { isOpen } = useModalStore();
+
+    return (
+        <>
+            <Dialog
+                open={isOpen}
+                as="div"
+                className="relative z-10 focus:outline-none "
+                onClose={() => {}}
+            >
+                <div className="fixed inset-0 z-10 w-screen overflow-y-auto backdrop-blur-sm">
+                    <div className="flex items-center justify-center min-h-full p-4 ">
+                        <DialogPanel
+                            transition
+                            className="w-3/5 p-8 border shadow-md bg-accent/10 border-accent/10"
+                        >
+                            <DialogTitle
+                                as="h3"
+                                className="text-xl font-medium text-center"
+                            >
+                                {title}
+                            </DialogTitle>
+                            <div className="divider"></div>
+                            <div>{children}</div>
+                        </DialogPanel>
+                    </div>
+                </div>
+            </Dialog>
+        </>
+    );
+};
