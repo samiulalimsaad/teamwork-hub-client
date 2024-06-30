871d93c5e421367370ab0ed9c03c507d67e4c5f7
samiulalimsaad <samiulalimsaad@gmail.com>
2024-07-01 01:18:16 +0600

    Version hooks added

---
+++ src/services/hooks/version.ts
@@ -0,0 +1,69 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { VersionInterface } from "../../interfaces/Version.interface";
+import {
+    createVersion,
+    deleteVersion,
+    fetchVersionById,
+    fetchVersions,
+    updateVersion,
+} from "../api/version";
+
+export const useFetchVersions = (projectId: string) => {
+    return useQuery({
+        queryKey: ["Versions", projectId],
+        queryFn: () => fetchVersions(projectId),
+    });
+};
+
+export const useFetchVersionById = (id: string) => {
+    return useQuery({
+        queryKey: ["Version", id],
+        queryFn: () => fetchVersionById(id),
+    });
+};
+
+export const useCreateVersion = () => {
+    const queryClient = useQueryClient();
+    const navigate = useNavigate();
+
+    return useMutation({
+        mutationFn: createVersion,
+        onSuccess: ({ data }) => {
+            queryClient.invalidateQueries({ queryKey: ["Versions"] });
+            toast.success("Version added successfully!");
+            navigate(`/project/Version/${data._id}`);
+        },
+    });
+};
+
+export const useUpdateVersion = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: ({
+            id,
+            updatedVersion,
+        }: {
+            id: string;
+            updatedVersion: Partial<VersionInterface>;
+        }) => updateVersion(id, updatedVersion),
+        onSuccess: (_, { id }) => {
+            queryClient.invalidateQueries({ queryKey: ["Versions"] });
+            queryClient.invalidateQueries({ queryKey: ["Version", id] });
+        },
+    });
+};
+
+export const useDeleteVersion = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: (id: string) => deleteVersion(id),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["Versions"] });
+            toast.success("Version deleted successfully!");
+        },
+    });
+};
