68d74b9d18f8b379aff92a53274dd0ae8eb85e28
samiulalimsaad <samiulalimsaad@gmail.com>
2024-07-01 00:36:31 +0600

    grouped by email

--- src/interfaces/Project.interface.ts
+++ src/interfaces/Project.interface.ts
@@ -1,8 +1,10 @@
 import { DocumentInterface } from "./Document.interface";
+import { UserInterface } from "./User.interface";
 import { CommonInterface } from "./common.interface";
 
 export interface ProjectInterface extends CommonInterface {
     title: string;
     description: string;
+    createdBy: UserInterface;
     documents: DocumentInterface[];
 }
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,23 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import EditProject from "../components/Project/EditProject";
 import NewProject from "../components/Project/NewProject";
 import { ProjectInterface } from "../interfaces/Project.interface";
+import { useAuth } from "../providers/hooks/auth";
 import { useDeleteProject, useFetchProjects } from "../services/hooks/project";
 import { Modal } from "../utils/ui/Modal";
 
 const Projects: React.FC = () => {
+    const { user } = useAuth();
     const { data: projects } = useFetchProjects();
     const [isOpen, setIsOpen] = useState(false);
     const [editing, setEditing] = useState<ProjectInterface>();
+    const [isAnimating, setIsAnimating] = useState<boolean>(true);
+
+    useEffect(() => {
+        const tout = setTimeout(() => {
+            setIsAnimating(false);
+        }, 4500);
+
+        return () => {
+            clearTimeout(tout);
+        };
+    }, []);
+
     const deleteProject = useDeleteProject();
 
     return (
         <div>
             <div className="flex items-center justify-between p-4 mt-8 bg-accent/10">
-                <h3 className="text-xl text-center ">Projects</h3>
+                <h3 className="text-xl text-center drop-shadow-md">Projects</h3>
                 <button
-                    className="btn btn-accent btn-xs"
+                    className={`btn btn-accent btn-xs ${
+                        isAnimating && !isOpen ? "animate-bounce" : ""
+                    }`}
                     onClick={() => setIsOpen(true)}
                 >
                     New
@@ -25,48 +41,110 @@ const Projects: React.FC = () => {
             </div>
             <div className="divider"></div>
             <div>
+                <h4 className="text-xl drop-shadow-md">My projects:</h4>
+
                 <ul className="p-2 mx-auto my-8 space-y-8 border card rounded-box border-accent/10">
-                    {projects?.data?.map((project) => (
-                        <li key={project._id}>
-                            <div className="p-2 mx-auto hover:bg-accent/10">
-                                <div className="flex items-center justify-between">
-                                    <div>
-                                        <h3 className="text-xl">
-                                            {project.title}
-                                        </h3>
-                                        <p className="text-xs opacity-40">
-                                            {project.description}
-                                        </p>
-                                    </div>
-                                    <div className="flex items-center gap-4">
-                                        <button
-                                            className="btn btn-error btn-xs"
-                                            onClick={() =>
-                                                deleteProject.mutate(
-                                                    project._id
-                                                )
-                                            }
-                                        >
-                                            Delete
-                                        </button>
-                                        <button
-                                            className="btn btn-warning btn-xs"
-                                            onClick={() => setEditing(project)}
-                                        >
-                                            Edit
-                                        </button>
-                                        <Link
-                                            to={`/project/${project._id}`}
-                                            className="btn btn-accent btn-xs"
-                                        >
-                                            details
-                                        </Link>
+                    {projects?.data
+                        ?.filter((p) => p.createdBy.email === user?.email)
+                        ?.map((project) => (
+                            <li key={project._id}>
+                                <div className="p-2 mx-auto hover:bg-accent/10">
+                                    <div className="flex flex-col items-center justify-between gap-4 sm:flex-row">
+                                        <div>
+                                            <h3 className="text-xl drop-shadow-md">
+                                                {project.title}
+                                            </h3>
+                                            <p className="text-xs opacity-40">
+                                                {project.description}
+                                            </p>
+                                        </div>
+                                        <div className="flex items-center gap-4">
+                                            <button
+                                                className="btn btn-error btn-xs"
+                                                onClick={() =>
+                                                    deleteProject.mutate(
+                                                        project._id
+                                                    )
+                                                }
+                                            >
+                                                Delete
+                                            </button>
+                                            <button
+                                                className="btn btn-warning btn-xs"
+                                                onClick={() =>
+                                                    setEditing(project)
+                                                }
+                                            >
+                                                Edit
+                                            </button>
+                                            <Link
+                                                to={`/project/${project._id}`}
+                                                className="btn btn-accent btn-xs"
+                                            >
+                                                details
+                                            </Link>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="my-0 divider before:bg-accent/20 after:bg-accent/20"></div>
-                        </li>
-                    ))}
+                                <div className="my-0 divider before:bg-accent/20 after:bg-accent/20"></div>
+                            </li>
+                        ))}
+                </ul>
+            </div>
+            <div>
+                <h4 className="text-xl drop-shadow-md">Others projects:</h4>
+                <ul className="p-2 mx-auto my-8 space-y-8 border card rounded-box border-accent/10">
+                    {projects?.data
+                        ?.filter((p) => p.createdBy.email !== user?.email)
+                        .map((project) => (
+                            <li key={project._id}>
+                                <div className="p-2 mx-auto hover:bg-accent/10">
+                                    <div className="flex flex-col items-center justify-between gap-4 sm:flex-row">
+                                        <div>
+                                            <div className="flex items-center gap-4">
+                                                <h3 className="text-xl drop-shadow-md">
+                                                    {project.title}
+                                                </h3>
+                                                <small className="text-xs opacity-40">
+                                                    createdBy:{" "}
+                                                    {project.createdBy.name}
+                                                </small>
+                                            </div>
+                                            <p className="text-xs opacity-40">
+                                                {project.description}
+                                            </p>
+                                        </div>
+                                        <div className="flex items-center gap-4">
+                                            <button
+                                                className="btn btn-error btn-xs"
+                                                onClick={() =>
+                                                    deleteProject.mutate(
+                                                        project._id
+                                                    )
+                                                }
+                                            >
+                                                Delete
+                                            </button>
+                                            <button
+                                                className="btn btn-warning btn-xs"
+                                                onClick={() =>
+                                                    setEditing(project)
+                                                }
+                                            >
+                                                Edit
+                                            </button>
+                                            <Link
+                                                to={`/project/${project._id}`}
+                                                className="btn btn-accent btn-xs"
+                                            >
+                                                details
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="my-0 divider before:bg-accent/20 after:bg-accent/20"></div>
+                            </li>
+                        ))}
                 </ul>
             </div>
             <Modal title="Create a new Project?" isOpen={isOpen}>
