62acb267b666f6c2120e6ed897406fe8d4a00401
samiulalimsaad <samiulalimsaad@gmail.com>
2024-07-01 02:58:05 +0600

    restore option added

--- src/components/Document/Diff.tsx
+++ src/components/Document/Diff.tsx
@@ -1,8 +1,14 @@
 import { DiffEditor } from "@monaco-editor/react";
 import React from "react";
 import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 import { VersionInterface } from "../../interfaces/Version.interface";
-import { useFetchDocumentById } from "../../services/hooks/document";
+import {
+    useFetchDocumentById,
+    useUpdateDocument,
+} from "../../services/hooks/document";
+import { useDeleteVersion } from "../../services/hooks/version";
+import Error from "../../utils/ui/Error";
 
 interface VersionProps {
     version: VersionInterface;
@@ -12,8 +18,13 @@ interface VersionProps {
 const Diff: React.FC<VersionProps> = ({ version, close }) => {
     const { id } = useParams();
     const { data: document } = useFetchDocumentById(id!);
+    const deleteVersion = useDeleteVersion();
+    const updateDocument = useUpdateDocument();
     return (
-        <div>
+        <div className="w-full">
+            <div className="md:hidden">
+                <Error error="For better view Please use fullscreen" />
+            </div>
             <div>
                 <div className="flex items-center justify-around mb-2 font-semibold drop-shadow-md">
                     <span>Now</span>
@@ -28,9 +39,26 @@ const Diff: React.FC<VersionProps> = ({ version, close }) => {
                     modified={version.document.content}
                 />
             </div>
-            <div className="mx-auto my-4">
-                <button className="btn btn-info" onClick={close}>
+            <div className="flex items-center justify-end gap-4 my-4">
+                <button className=" btn btn-info" onClick={close}>
                     Close
+                </button>
+                <button
+                    className=" btn btn-warning"
+                    onClick={async () => {
+                        await updateDocument.mutateAsync({
+                            id: id!,
+                            updatedDocument: {
+                                title: version.document.title,
+                                content: version.document.content,
+                            },
+                        });
+                        toast.success("This version restored");
+                        await deleteVersion.mutateAsync(version._id);
+                        close();
+                    }}
+                >
+                    Restore
                 </button>
             </div>
         </div>
