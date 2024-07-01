c45e50a2d2d0e9e8251729e2e698f561f4cc6e21
samiulalimsaad <samiulalimsaad@gmail.com>
2024-07-01 16:45:55 +0600

    from env

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -1,5 +1,3 @@
-// export const URL = "https://teamwork-hub-server.vercel.app";
-export const URL = "https://teamwork-hub-server.onrender.com";
-// export const URL = "http://localhost:5000";
+export const URL = import.meta.env.VITE_API_URL;
 export const BASE_URL = `${URL}/api`;
 export const BASE_URL_SOCKET = URL;
